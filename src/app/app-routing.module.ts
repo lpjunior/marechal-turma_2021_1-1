9c2ee4887da01f462839c796238f7ea2a67bcafa
Luis Paulo Lessa <prof.lpjunior@gmail.com>
2022-10-17 18:08:10 -0300

    feat: projeto com angular router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './todo/create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'todo/create',
+    component: CreateComponent
+  },
+];
+
+// criação(create) -> /create
+// edição(edit) -> /edit
+// listagem(list) -> /list
+// visuação(view) -> /{id}
+// compartilhamento(shared) /{id}/shared
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './todo/create/create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonToggleModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
