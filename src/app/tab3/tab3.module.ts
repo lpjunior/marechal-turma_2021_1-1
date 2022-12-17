b8373e758d452d30d766287528e462d176e943f2
Luis Lessa <prof.lpjunior@gmail.com>
2022-12-16 21:26:30 -0300

    feat: implementação do filtro

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,18 +1,21 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
+import { FirebaseService } from '../services/firebase.service';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     Tab3PageRoutingModule
   ],
-  declarations: [Tab3Page]
+  declarations: [Tab3Page],
+  providers: [FirebaseService]
 })
 export class Tab3PageModule {}
